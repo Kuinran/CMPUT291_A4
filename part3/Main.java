fdea1d1dce3628cdd8ece4e24ce6d0d26cb02256
Michael Huang <chuang4@ualberta.ca>
2018-11-24 16:26:33 -0700

    added something

--- part3/Main.java
+++ part3/Main.java
@@ -1,5 +1,14 @@
-import com.sleepycat.je.*;
+import com.sleepycat.db.*;
 
 public class Main {
-
+	
+	public static void Main() {
+		Database all[] = null;
+		try {
+			all = OpenDB.Open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
