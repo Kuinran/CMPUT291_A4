779568614f4f4aabefd33412e25f06aba081f20b
Cheng Huang <chuang4@cs.ualberta.ca>
2018-11-25 12:11:24 -0700

    orginized

--- part2/Load.sh
+++ part2/Load.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-sort -V -u ads.txt -o ads.txt # | uniq
-sort -V -u terms.txt -o terms.txt # | uniq
-sort -V -u prices.txt -o prices.txt # | uniq
-sort -V -u pdates.txt -o pdates.txt # | uniq
+sort -V -u ../part1/ads.txt -o ads.txt # | uniq
+sort -V -u ../part1/terms.txt -o terms.txt # | uniq
+sort -V -u ../part1/prices.txt -o prices.txt # | uniq
+sort -V -u ../part1/pdates.txt -o pdates.txt # | uniq
 
 cat ads.txt | ./break.pl | db_load -T -t hash ad.idx
 cat terms.txt | ./break.pl | db_load -c duplicates=1 -T -t btree te.idx
 cat prices.txt | ./break.pl | db_load -c duplicates=1 -T -t btree pr.idx
 cat pdates.txt | ./break.pl | db_load -c duplicates=1 -T -t btree da.idx
+
+cp ad.idx ../part3
+cp te.idx ../part3
+cp pr.idx ../part3
+cp da.idx ../part3
